5fe920f5151f72a2b8e90d43c3382f4c1b040edf
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2021-09-06 09:58:24 +0200

    + OOP Övning

---
+++ OOP Övning/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Övning
+{
+    class Car
+    {
+        public string _brand;
+        public string _color;
+    }
+}
---
+++ OOP Övning/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOP_Övning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            Car car2 = new Car();
+            car1._brand = "Audi";
+            car1._color = "White";
+            car2._brand = "Volvo";
+            car2._color = "Black";
+            Console.WriteLine($"{car1._color} {car1._brand}");
+            Console.WriteLine($"{car2._color} {car2._brand}");
+
+            Console.ReadKey();
+        }
+    }
+}
