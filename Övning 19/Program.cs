b8d406604933e1a4419ec2797ecae25c280ab227
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2021-09-06 11:10:02 +0200

    +Övning 19

---
+++ Övning 19/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Övning_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Anropa själva boxen
+            DrawBox(7, 4);
+
+
+            Console.ReadKey();
+        }
+
+
+        //Skriv en metod DrawBox(int width, int height)
+        public static void DrawBox(int width, int height)
+        {
+            //Töm konsollen med Console.Clear()
+            Console.Clear();
+
+            //Skapar raden
+            string line = new('#', width);
+            
+            //Skapar inre raderna
+            string innerLine = "";
+            innerLine += "#";
+
+            //Loopar igenom bredden
+            for (int j = 0; j < width - 2; j++)
+            {
+                innerLine += "-";
+            }
+            innerLine += "#";
+            Console.WriteLine(line);
+
+            //Loopar igenom höjden
+            for (int i = 0; i < height - 2; i++)
+            {
+                Console.WriteLine(innerLine);
+            }
+            Console.WriteLine(line);
+
+        }
+    }
+}
