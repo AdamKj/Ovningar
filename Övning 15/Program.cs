af589f6b4e5363c6efc6661949cf20a1eafac9c4
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2021-09-08 16:15:10 +0200

    +övning 15 +övning 16, ej klar

---
+++ Övning 15/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Övning_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Be användaren skriva in valfritt ord
+            Console.WriteLine("Skriv in ett valfritt ord");
+            string input = Console.ReadLine();
+
+            //Kolla om ordet är ett palindrom eller inte och skriv ut det
+            string reverse = "";
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reverse += input[i];
+            }
+
+            if (input == reverse)
+            {
+                Console.WriteLine($"Ordet {input} är ett palindrom!");
+            }
+            else
+            {
+                Console.WriteLine($"Ordet {input} är inte ett palindrom.");
+            } 
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Övning 16/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Övning_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Gör om uppgift 6, enkel miniräknare, så att man kan mata in allt på samma rad
+            //Fråga användaren efter ett tal
+            Console.WriteLine("Skriv in ett tal");
+            double inputOne = double.Parse(Console.ReadLine());
+
+            //Fråga sedan efter ett av följande +,-,*,/
+            Console.WriteLine("Skriv nu in ett av följande:");
+            Console.WriteLine("+");
+            Console.WriteLine("-");
+            Console.WriteLine("*");
+            Console.WriteLine("/");
+            string operand = Console.ReadLine();
+
+            //Fråga användaren efter ett andra tal
+            Console.WriteLine("Skriv nu in ett till tal");
+            double inputTwo = double.Parse(Console.ReadLine());
+
+            double result = 0;
+            switch (operand)
+            {
+                case "+":
+                    result = inputOne + inputTwo;
+                    break;
+                case "-":
+                    result = inputOne - inputTwo;
+                    break;
+                case "*":
+                    result = inputOne * inputTwo;
+                    break;
+                case "/":
+                    result = inputOne / inputTwo;
+                    break;
+                default:
+                    Console.WriteLine("Vänligen skriv bara in ett av följande +,-,*,/");
+                    break;
+            }
+
+            //Slå sedan ihop summan av de inmatade talen och tecknet och skriv ut, te x. 3 * 5 = 15
+            Console.WriteLine($"Resultatet av talen blev {inputOne} {operand} {inputTwo} = {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
