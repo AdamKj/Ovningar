3e819bb65c1589a8d8f813982361c3e08e09235b
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2021-09-09 16:04:53 +0200

    övning 16 klar +övning 17

--- Övning 16/Program.cs
+++ Övning 16/Program.cs
@@ -7,40 +7,65 @@ namespace Övning_16
     {
         static void Main(string[] args)
         {
-            //Gör om uppgift 6, enkel miniräknare, så att man kan mata in allt på samma rad te x. 3 + 5
-
-            double number = 0;
-            double result = 0;
-            var operators = new List<char> {'+', '-', '*', '/' };
+            //Gör om uppgift 6, enkel miniräknare, så att man kan mata in allt på samma rad te x. 3 * 5
 
             //Fråga användaren efter ett tal
-            Console.WriteLine("Skriv in ett tal");
-            var input = double.TryParse(Console.ReadLine(), out number);
+            Console.WriteLine("Skriv in valfri ekvation");
+            var input = Console.ReadLine();
 
-            if (input == operators.Contains('+'))
-            {
-                number += result;
-            }
-            else if (input == operators.Contains('-'))
-            {
-                number -= result;
-            }
-            else if (input == operators.Contains('*'))
-            {
-                number *= result;
-            }
-            else
-            {
-                number /= result;
-            }
-
-            
-
+            var operands = new char[] { '+', '-', '*', '/' };
+            var numbers = input.Split(operands);
 
             //Slå sedan ihop summan av de inmatade talen och tecknet och skriv ut, te x. 3 * 5 = 15
-            Console.WriteLine($"Resultatet av talen blev {number}");
+            foreach (var operand in operands)
+            {
+                if (input.Contains(operand)) 
+                {
+                    Console.WriteLine($"{numbers[0].Trim()} {operand} {numbers[1].Trim()} = {PerformOperation(numbers, operand)}");
+                }
+            }
 
             Console.ReadKey();
+        }
+
+        static double PerformOperation(string[] numbers, char operand)
+        {
+            double result = 0;
+            switch (operand)
+            {
+                case '+':
+                    result = Addition(double.Parse(numbers[0].Trim()), double.Parse(numbers[1].Trim()));
+                    break;
+                case '-':
+                    result = Sub(double.Parse(numbers[0].Trim()), double.Parse(numbers[1].Trim()));
+                    break;
+                case '*':
+                    result = Multiply(double.Parse(numbers[0].Trim()), double.Parse(numbers[1].Trim()));
+                    break;
+                case '/':
+                    result = Divide(double.Parse(numbers[0].Trim()), double.Parse(numbers[1].Trim()));
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+        private static double Divide(double firstNumber, double secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+        private static double Multiply(double firstNumber, double secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+        private static double Sub(double firstNumber, double secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        private static double Addition(double firstNumber, double secondNumber)
+        {
+            return firstNumber + secondNumber;
         }
     }
 }
---
+++ Övning 17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Övning_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Be användaren mata in en text tex. abcdefghijklmnopqrstuvwxyzåäö
+            //Be användaren mata in en delsträng i den först inmatningen tex. defg
+            //Markera den delsträngen i den första texten med en annan färg
+            //Skriv ut hela texten med delsträngen markerad
+        }
+    }
+}
