b7773a05062d6304cf8e0d54754fcdb79d7bb373
Adam Kjellberg <ak.98@live.se>
2021-09-22 10:21:30 +0200

    --

--- WPFDemo/MainWindow.xaml.cs
+++ WPFDemo/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WPFDemo
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Output.Text += "Hejsan";
+        }
     }
 }
